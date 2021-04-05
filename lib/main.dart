272f0e5c9c397e05683979f87a0016f0093eb11c
a30 <alperotuzoglu@gmail.com>
2021-04-05 07:49:37 +0300

    Firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:user_login/screens/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
